bf6dfcb4e7a5543267c22331d3841f0d21f24009
Cathy Le <cathyle@cathys-mbp.lan>
2021-10-26 02:08:58 -0400

    figured out to add react-sticky component

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -24,45 +24,58 @@ class Work extends Component {
     // console.log("hello");
 
     return (
-      <div className="work-section clearfix" id="work-section">
-        {/* <div className="bckgrd-white title-section"> */}
-        <div className="bckgrd-white title-section">
-          <div className="title-container">
-            <h1>Work</h1>
-          </div>
-          <Menu />
-        </div>
-        <div className="projects-section">
-          <div
-            className="project-item-container project-1"
-            style={{
-              backgroundImage: `url(/static/media/project-gmmb.3e577fe9.jpeg)`,
-            }}
-          >
-            <div className="project-content">
-              <h2>GMMB</h2>
-              <h6>
-                Interactive and creative website using parallax, animations, and
-                hover effects
-              </h6>
+      <StickyContainer>
+        <div className="work-section clearfix" id="work-section">
+          {/* <Sticky>
+            <div className="bckgrd-white title-section">
+              <div className="title-container">
+                <h1>Work</h1>
+              </div>
+              <Menu />
+            </div>
+          </Sticky> */}
+          <Sticky disableCompensation>
+            {({ style }) => (
+              <div style={style} className="bckgrd-white title-section">
+                <div className="title-container">
+                  <h1>Work</h1>
+                </div>
+                <Menu />
+              </div>
+            )}
+          </Sticky>
+          <div className="projects-section">
+            <div
+              className="project-item-container project-1"
+              style={{
+                backgroundImage: `url(/static/media/project-gmmb.3e577fe9.jpeg)`,
+              }}
+            >
+              <div className="project-content">
+                <h2>GMMB</h2>
+                <h6>
+                  Interactive and creative website using parallax, animations,
+                  and hover effects
+                </h6>
+              </div>
+            </div>
+            <div
+              className="project-item-container project-2"
+              style={{
+                backgroundImage: `url(/static/media/project-acaai.fa048b77.jpeg)`,
+              }}
+            >
+              <div className="project-content">
+                <h2>American College of Allergy, Asthma, & Immunology</h2>
+                <h6>
+                  Interactive and creative website using parallax, animations,
+                  and hover effects
+                </h6>
+              </div>
             </div>
           </div>
-          <div
-            className="project-item-container project-2"
-            style={{
-              backgroundImage: `url(/static/media/project-acaai.fa048b77.jpeg)`,
-            }}
-          >
-            <div className="project-content">
-              <h2>American College of Allergy, Asthma, & Immunology</h2>
-              <h6>
-                Interactive and creative website using parallax, animations, and
-                hover effects
-              </h6>
-            </div>
-          </div>
         </div>
-      </div>
+      </StickyContainer>
     );
   }
 }
