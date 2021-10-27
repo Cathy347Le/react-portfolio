d3dfebfd8b7153993f3e06bc3e663145fc466629
Cathy Le <cathyle@cathys-mbp.lan>
2021-10-27 02:05:32 -0400

    add more projects

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -47,6 +47,38 @@ class Work extends Component {
           backgroundImage: "../images/project-elections.jpg",
           linkURL: "http://project-5-election-site.surge.sh/",
         },
+        {
+          id: 6,
+          title: "Petz",
+          description:
+            'A social media web app for pets. Users can create a profile for their pets, post comments, and "lick" other pet profiles. Built using React, MongoDB, Express, and Node.js. Front-end deployed on Surge and Backend deployed on Heroku. It was built alongside 2 other classmates.',
+          backgroundImage: "../images/project-elections.jpg",
+          linkURL: "https://github.com/Cathy347Le/project3frontend",
+        },
+        {
+          id: 7,
+          title: "Think Movie",
+          description:
+            "Allows users to create movie lists to manage movies they love, want to watch, or just to catalogue their entire movie collection. Built with Express, Handlebars, Node.js, and MongoDB. Deployed on Heroku.",
+          backgroundImage: "../images/project-rewire.jpeg",
+          linkURL: "https://thinkmovie.herokuapp.com/",
+        },
+        {
+          id: 8,
+          title: "Wags to Wings",
+          description:
+            "Online store for pet supplies. The e-commerce website was built using Python, Django, and Bootstrap during a two-day, in-class Django Hackathon, where we had to develop a web app based on a UX design mockup. It was built alongside 3 other classmates.",
+          backgroundImage: "../images/project-rewire.jpeg",
+          linkURL: "https://github.com/Cathy347Le/MangoDjango",
+        },
+        {
+          id: 9,
+          title: "Lunar New Year Jeopardy",
+          description:
+            "Self-scoring trivia game utilizing JavaScript, HTML, and CSS",
+          backgroundImage: "../images/project-rewire.jpeg",
+          linkURL: "https://cathy347le.github.io/Lunar-New-Year-Jeopardy/",
+        },
       ],
     };
   }
