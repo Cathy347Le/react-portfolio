ef2b82c8e24c10c3573392c8418afee8ae02ad08
Cathy Le <cathyle@cathys-mbp.lan>
2021-10-27 01:40:50 -0400

    work on contact section content

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,21 +15,23 @@ class Contact extends Component {
           </div>
         </div>
         <div className="col-xs-12 col-sm-6 bckgrd-white">
-          <h3>I love meeting new people</h3>
-          <h3>just say hello.</h3>
-          <p>
-            I’m always open to discussing new projects, creative ideas or
-            opportunities to be a part of your team. Feel free to email me.
-          </p>
-          <h4>Cathy Le</h4>
-          <p>Software Engineer</p>
-          <a
-            href="mailto:cathy347le@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Cathy347Le@gmail.com
-          </a>
+          <div className="content-container">
+            <h3>I love meeting new people</h3>
+            <h3>just say hello.</h3>
+            <p>
+              I’m always open to discussing new projects, creative ideas or
+              opportunities to be a part of your team. Feel free to email me.
+            </p>
+            <h3 className="signature">Cathy Le</h3>
+            <p className="title">Software Engineer</p>
+            <a
+              href="mailto:cathy347le@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Cathy347Le@gmail.com
+            </a>
+          </div>
         </div>
       </div>
     );
