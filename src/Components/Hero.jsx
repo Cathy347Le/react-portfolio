addfe869bad21a169b34b6ed36c7e8dfe1d0c6e9
Cathy Le <cathyle@cathys-mbp.lan>
2021-11-03 03:31:44 -0400

    work on hero section and responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import "./Hero.scss";
+import Menu from "./Menu";
 
 class Hero extends Component {
   render() {
     return (
       <div className="hero-section">
-        <h1>Hello</h1>
+        <div className="hero-animation-container">
+          <div className="box box1"></div>
+          <div className="box box2"></div>
+          <div className="box box3"></div>
+          <div className="box box4"></div>
+          <div className="box box5 main-box">
+            <h1>Hello5</h1>
+          </div>
+          <div className="box box6"></div>
+          <div className="box box7 hidden-box"></div>
+          <div className="box box8 hidden-box">
+            <Menu />
+          </div>
+          <div className="box box9 hidden-box"></div>
+        </div>
       </div>
     );
   }
