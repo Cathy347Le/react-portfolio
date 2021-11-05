10cd270fbc3344d985a26b56ea9e1a9553708361
Cathy Le <cathyle@cathys-mbp.lan>
2021-11-04 21:11:57 -0400

    get outer hero animation to work

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,6 +7,10 @@ class Hero extends Component {
     return (
       <div className="hero-section">
         <div className="outer-box">
+          <span className="outerlightbar bar1"></span>
+          <span className="outerlightbar bar2"></span>
+          <span className="outerlightbar bar3"></span>
+          <span className="outerlightbar bar4"></span>
           <div className="inner-box">
             <span className="innerlightbar bar1"></span>
             <span className="innerlightbar bar2"></span>
