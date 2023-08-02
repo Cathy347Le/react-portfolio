fdc794d224e034b23cc980c2832da35af709d3b8
Cathy Le <cathy347le@gmail.com>
2023-08-01 23:31:01 -0400

    Destructure props

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -2,22 +2,19 @@ import React, { Component } from 'react';
 
 class Project extends Component {
   render() {
+    const { backgroundImage, description, linkURL, title } = this.props.project;
     return (
       <div
         className="project-item-container"
         style={{
-          backgroundImage: `url(${this.props.project.backgroundImage})`,
+          backgroundImage: `url(${backgroundImage})`,
         }}
       >
         <div className="project-content">
-          <h3>{this.props.project.title}</h3>
-          <h4 className="description">{this.props.project.description}</h4>
+          <h3>{title}</h3>
+          <h4 className="description">{description}</h4>
           <div className="button-container">
-            <a
-              href={this.props.project.linkURL}
-              target="_blank"
-              rel="noreferrer"
-            >
+            <a href={linkURL} target="_blank" rel="noreferrer">
               <span></span>
               <span></span>
               <span></span>
